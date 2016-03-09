509a47ee9f4180230702b1049a40385e1ddcae47
Ibrahim Eid <humazed@gmail.com>
2016-03-09 06:38:40 +0200

    feature #2 add more work

--- src/com/company/Feature2.java
+++ src/com/company/Feature2.java
@@ -3,4 +3,7 @@ package com.company;
 public class Feature2 {
 
     //working on feature 3
+
+    //adding new work on feature #2
+
 }
--- src/com/company/NewFeature.java
+++ src/com/company/NewFeature.java
@@ -3,4 +3,7 @@ package com.company;
 public class NewFeature {
 
     //added new feature
+
+    //adding new work on new feature
+
 }
